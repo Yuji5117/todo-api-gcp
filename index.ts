2005e1e68d177d2aebdc848f43ac77890ee6533d
Yuji Nakamura <ny5117@icloud.com>
2025-05-27 08:15:27 +1200

    Modify port and response

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { pool } from "./config/db";
 
-const port = 8000;
+const port = 8080;
 const app = express();
 
 app.get("/todos", async (_, res) => {
   try {
-    const result = await pool.query("SELECT * FROM todo");
-    res.json(result.rows);
+    // const result = await pool.query("SELECT * FROM todo");
+    // res.json(result.rows);
+    res.send("Hello!!");
   } catch (err) {
     res.status(500).send("Server error");
   }
