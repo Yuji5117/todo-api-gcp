e2a134ee7e73b075d2e4f34ee8dc0d01e63b39e3
Yuji Nakamura <ny5117@icloud.com>
2025-05-28 06:05:52 +1200

    Modify response text

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ app.get("/todos", async (_, res) => {
   try {
     // const result = await pool.query("SELECT * FROM todo");
     // res.json(result.rows);
-    res.send("Update!!");
+    res.send("Check!!");
   } catch (err) {
     res.status(500).send("Server error");
   }
