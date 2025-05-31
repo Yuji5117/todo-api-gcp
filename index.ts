267ea9b30d37f5518951c7c3698839260f67f2b3
Yuji Nakamura <ny5117@icloud.com>
2025-06-01 07:18:47 +1200

    Remove unused code

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
 import express from "express";
-import { pool } from "./config/db";
 
 const port = 8000;
 const app = express();
 
 app.get("/todos", async (_, res) => {
   try {
-    // const result = await pool.query("SELECT * FROM todo");
-    // res.json(result.rows);
     res.send("Change Again to test the deployment");
   } catch (err) {
     res.status(500).send("Server error");
