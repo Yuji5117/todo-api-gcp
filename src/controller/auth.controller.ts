16b93f36f63116ab7c2a5dcc4a8b5f34941d50ef
Yuji Nakamura <ny5117@icloud.com>
2025-06-04 17:11:59 +1200

    remove unused comment

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -22,31 +22,29 @@ const users: User[] = [
 const JWT_SECRET = process.env.JWT_SECRET || "jwt-secret-key";
 
 export const register = async (req: Request, res: Response) => {
-  // extract  email and password from request
   const { email, password } = req.body;
-  // return 400 bad request if either email or password is missing
+
   if (!email || !password) {
     res.status(400).json({ message: "Email and Password are required." });
     return;
   }
-  // check if the email already exists or not
+
   const isExist = users.some((user) => user.email === email);
-  // if exists then throw err
+
   if (isExist) {
     res.status(409).json({ message: "User already exists." });
     return;
   }
 
-  // Hash the email address
   const hashedPassword = await bcrypt.hash(password, 10);
-  // store them into database
+
   const newUser = {
     id: users.length + 1,
     email: email,
     password: hashedPassword,
   };
   users.push(newUser);
-  // return  email and password
+
   res.status(201).json({
     message: "User registered successfully.",
     user: { id: newUser.id, email: newUser.email },
@@ -54,35 +52,32 @@ export const register = async (req: Request, res: Response) => {
 };
 
 export const login = async (req: Request, res: Response) => {
-  // Extract email and password from req
   const { email, password } = req.body;
-  // return 400 bad request if either email or password is missing
+
   if (!email || !password) {
     res.status(400).json({ message: "Email and Password are required." });
     return;
   }
-  // search user by email
+
   try {
     const user = users.find((user) => user.email === email);
     if (!user) {
       res.status(401).json({ message: "Invalid email or password." });
       return;
     }
-    // check password
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       res.status(401).json({ message: "Invalid email or password." });
       return;
     }
 
-    // JWT
     const jwtToken = jwt.sign(
       { userId: user.id, email: user.email },
       JWT_SECRET,
       { expiresIn: "1h" }
     );
 
-    // response 200 with message
     res.status(200).json({ message: "Login successful.", token: jwtToken });
   } catch (error) {
     console.log("Login error:", error);
