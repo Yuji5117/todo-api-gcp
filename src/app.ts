a0e8b835b63bcbbd078169d7de222331b27814e3
Yuji Nakamura <ny5117@icloud.com>
2025-06-08 08:28:05 +1200

    Remove unused code

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import express from "express";
 import authRouter from "./route/auth.route";
 import teamRouter from "./route/team.route";
-import { authenticateToken } from "./middleware/auth.middleware";
 import { errorHandler } from "./middleware/errorHandler";
-import { AppError } from "./utils/AppError";
 
 const app = express();
-const tasks = require("./data/task");
 
 app.use(express.json());
 
