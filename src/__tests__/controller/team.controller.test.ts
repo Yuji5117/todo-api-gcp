f76b62bf5eb1c9cfed7c4b7aaf24de19a7d259e9
Yuji Nakamura <ny5117@icloud.com>
2025-06-27 15:05:05 +0800

    add unit test for team controller

--- src/__tests__/controller/team.controller.test.ts
+++ src/__tests__/controller/team.controller.test.ts
@@ -1,5 +1,17 @@
-describe("My Test", () => {
-  it("should work", () => {
-    expect(1 + 1).toBe(2);
+import { getAllTeamController } from "../../controller/team.controller";
+import { AppError } from "../../utils/AppError";
+
+describe("getAllTeamController", () => {
+  const mockRes = {
+    json: jest.fn(),
+    status: jest.fn().mockReturnThis(),
+  } as any;
+
+  it("should throw AppError if userId is missing", () => {
+    const reqWithoutUserId = {} as any;
+
+    expect(getAllTeamController(reqWithoutUserId, mockRes)).rejects.toThrow(
+      new AppError("userId is required.", 400)
+    );
   });
 });
